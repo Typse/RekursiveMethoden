c22085bf14392f1733f2019db19b0eb94ebdfe19
G.SO <36795023+Typse@users.noreply.github.com>
2024-01-07 16:46:02 +0100

    fertig

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
+    
+    public static int fakultaetBerechnen(int input) {
+        if (input <= 0) {
+            return 1;
+        }
+        return input * fakultaetBerechnen(input - 1);
+    }
+
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(fakultaetBerechnen(0));
     }
 }
